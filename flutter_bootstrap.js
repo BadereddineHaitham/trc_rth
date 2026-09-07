bedf9052d4d99f0adf98cab2c745d7d5c8f3def0
Haitham <haithambadereddine.dz@gmail.com>
2026-09-07 12:36:59 +0100

    deploy: update web build with iOS PDF print fix for USD and Pompe

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2237613385"
+    serviceWorkerVersion: "540841897"
   }
 });
